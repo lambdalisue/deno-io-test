10079f294eeccc43d4e5a9e19e22ad8f3d0b0257
Alisue <lambdalisue@hashnote.net>
2021-08-23 10:20:53 +0900

    Fix

--- gen.ts
+++ gen.ts
@@ -1,9 +1,10 @@
 import { parse } from "https://deno.land/std@0.105.0/flags/mod.ts";
+import * as io from "https://deno.land/std@0.100.0/io/mod.ts";
 
 const opts = parse(Deno.args)
 const size = opts.size || 128;
 const v = "x".repeat(1024 * size);
 const b = new TextEncoder().encode(`["${v}"]`);
 
-await Deno.stdout.write(b);
+await io.writeAll(Deno.stdout, b);
 console.log("");
